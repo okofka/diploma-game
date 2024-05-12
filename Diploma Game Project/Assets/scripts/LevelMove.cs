789ccc2af7b6b388a16e86c25806b144cde07f5d
okofka <nazar.dmytroo@gmail.com>
2024-05-12 19:33:06 +0300

    Normal saving level id

--- Diploma Game Project/Assets/scripts/LevelMove.cs
+++ Diploma Game Project/Assets/scripts/LevelMove.cs
@@ -1,35 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelMove : MonoBehaviour
 {
-    public int sceneBuildIndex;
+    private int sceneBuildIndex;
 
-    public void Start()
+    private void Start()
     {
-        Player player = FindObjectOfType<Player>();
-        Debug.Log("Player level: " + player.level);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            sceneBuildIndex = nextSceneIndex;
+        }
+        else
+        {
+            sceneBuildIndex = SceneManager.sceneCountInBuildSettings - 1;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Player player = FindObjectOfType<Player>();
-
-            if (sceneBuildIndex > PlayerPrefs.GetInt("level", 0))
-            {
-                PlayerPrefs.SetInt("level", sceneBuildIndex);
-                PlayerPrefs.Save();
-            }
-
-            player.level = PlayerPrefs.GetInt("level", 0); // Оновлення рівня гравця з PlayerPref
-            Debug.Log("Player level: " + player.level);
-
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
         }
     }
 }
-
--- Diploma Game Project/Assets/scripts/Player.cs
+++ Diploma Game Project/Assets/scripts/Player.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public int level { get; set; }
+    public int level;
 
     public void Start()
     {
+        level = SceneManager.GetActiveScene().buildIndex;
         Debug.Log("Player level: " + level);
     }
 }
